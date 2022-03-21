aba7a4bf87015fdfa35fba88cc67051e66bdb1c4
sean <seankennett@gmail.com>
2022-03-21 22:12:56 +0000

    Correcting cloud endpoint

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   clientId: 'cf90ff06-6bca-41f6-9077-209cb09746a7',
-  apiEndpoint: 'https://mvbidentitypocapi.azurewebsites.net/api'
+  apiEndpoint: 'https://musicvideobuilder.azurewebsites.net/api'
 };
