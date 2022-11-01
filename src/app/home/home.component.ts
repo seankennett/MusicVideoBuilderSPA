371e484502f408fb86ff68475f7ad1af7a9e4518
sean <seankennett@gmail.com>
2022-11-01 17:26:57 +0000

    Corrected homepage text

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,4 @@
-import { Component, Inject, OnInit } from '@angular/core';
-import { MSAL_GUARD_CONFIG, MsalGuardConfiguration, MsalService, MsalBroadcastService } from '@azure/msal-angular';
-import { InteractionStatus, PopupRequest, RedirectRequest } from '@azure/msal-browser';
-import { filter, Subject, takeUntil } from 'rxjs';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -10,38 +7,10 @@ import { filter, Subject, takeUntil } from 'rxjs';
 })
 export class HomeComponent implements OnInit {
 
-  constructor(@Inject(MSAL_GUARD_CONFIG) private msalGuardConfig: MsalGuardConfiguration,
-    private authService: MsalService,
-    private msalBroadcastService: MsalBroadcastService) { }
+  constructor() { }
 
-  private readonly _destroying$ = new Subject<void>();
   loginDisplay = false;
 
   ngOnInit(): void {
-    /**
-       * You can subscribe to MSAL events as shown below. For more info,
-       * visit: https://github.com/AzureAD/microsoft-authentication-library-for-js/blob/dev/lib/msal-angular/docs/v2-docs/events.md
-       */
-    this.msalBroadcastService.inProgress$
-      .pipe(
-        filter((status: InteractionStatus) => status === InteractionStatus.None),
-        takeUntil(this._destroying$)
-      )
-      .subscribe(() => {
-        this.setLoginDisplay();
-      });
-  }
-
-  setLoginDisplay() {
-    this.loginDisplay = this.authService.instance.getAllAccounts().length > 0;
-  }
-
-  login = (userFlowRequest?: RedirectRequest | PopupRequest) => {
-    this.authService.loginRedirect({ ...this.msalGuardConfig.authRequest, ...userFlowRequest } as RedirectRequest);
-  }
-
-  ngOnDestroy(): void {
-    this._destroying$.next(undefined);
-    this._destroying$.complete();
   }
 }
