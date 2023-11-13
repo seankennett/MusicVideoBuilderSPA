5385859df46c23bd515290cf18fc37506249ca86
seankennett <seankennettwork@gmail.com>
2023-11-13 22:32:35 +0000

    Bulked out help ready for some questions

--- src/app/help/help.component.ts
+++ src/app/help/help.component.ts
@@ -12,4 +12,12 @@ export class HelpComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  faqs: {question:string, answer:string}[] = [
+    {question: 'question 1', answer:'answer 1'},
+    {question: 'question 2', answer:'answer 2'},
+    {question: 'question 3', answer:'answer 3'},
+    {question: 'question 4', answer:'answer 4'},
+    {question: 'question 5', answer:'answer 5'}
+  ]
+
 }
